23dac925d6c22afd0aff3bc44b03a407179a008d
Utkarsh Verma <utkarshverma@Utkarshs-MacBook-Pro.local>
2024-01-14 23:48:14 +0530

    Adding lib file and tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use minigrep::Config;
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|_err| {
+        println!("Problem parsing the arguments");
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Error: {e}");
+        process::exit(1);
+    }
 }
