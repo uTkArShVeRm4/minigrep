1b0cda62dd0aebad708013b3be6fef9c55dda58a
Utkarsh Verma <utkarshverma@Utkarshs-MacBook-Pro.local>
2024-01-15 00:01:53 +0530

    added search functionality

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,20 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
-
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
 }
 
 pub struct Config {
